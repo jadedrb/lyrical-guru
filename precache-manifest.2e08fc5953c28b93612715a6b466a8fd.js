ce55bd0b06a214bb4b33039ca9b07256b4b2ca28
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-10-26 15:46:29 -0400

    Updates

---
+++ precache-manifest.2e08fc5953c28b93612715a6b466a8fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c414dd789437e28238d44a42732c0b",
+    "url": "/lyrical-guru/index.html"
+  },
+  {
+    "revision": "5732d1fd16918066f57b",
+    "url": "/lyrical-guru/static/css/main.73cba99c.chunk.css"
+  },
+  {
+    "revision": "e25f011563c3b90ee74b",
+    "url": "/lyrical-guru/static/js/2.7373f711.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyrical-guru/static/js/2.7373f711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5732d1fd16918066f57b",
+    "url": "/lyrical-guru/static/js/main.4bfffc32.chunk.js"
+  },
+  {
+    "revision": "9b0b13eec2870ed5db03",
+    "url": "/lyrical-guru/static/js/runtime-main.2dc7e8bb.js"
+  }
+]);
